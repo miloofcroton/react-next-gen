2b03007474965350a02386844d3f39fbb601e469
Jack Toumey <jack@toumey.io>
2019-10-23 14:47:07 -0700

    typescript is working

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ const withMDX = require('@next/mdx')({
 })
 
 module.exports = withMDX({
-	pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+	pageExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+    'md',
+    'mdx'
+  ],
 })
