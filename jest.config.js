4abd0937fccfa72d2bcad339e7aec3a8a8bbab9b
Jack Toumey <jack@toumey.io>
2019-10-23 18:19:25 -0700

    testing is working nicely now

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,21 @@ module.exports = {
     '<rootDir>/src',
     '<rootDir>/.storybook',
   ],
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(j|t)sx?$',
+  // setupFiles: ['<rootDir>/src/tests/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$',
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(j|t)sx?$',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
-    '^.+\\.tsx?$': 'babel-jest',
+    '^.+\\.(j|t)sx?$': 'babel-jest',
     // '^.+\\.tsx?$': 'ts-jest'
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir>assetsTransformer.js',
+    '\\.(md|mdx|html)$': '<rootDir>assetsTransformer.js',
+  },
   moduleFileExtensions: [
     'ts',
     'tsx',
---
+++ src/components/Counter/Counter.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Counter from './Counter';
+
+afterEach(() => {
+  window.localStorage.removeItem('count');
+});
+
+test('counter increments the count', () => {
+  const { container } = render(<Counter />);
+  const button = container.firstChild;
+  expect(button.textContent).toBe('0');
+  fireEvent.click(button);
+  expect(button.textContent).toBe('1');
+});
+
+test('reads and updates localStorage', () => {
+  window.localStorage.setItem('count', 3);
+  const { container } = render(<Counter />);
+  const button = container.firstChild;
+  expect(button.textContent).toBe('3');
+  fireEvent.click(button);
+  expect(button.textContent).toBe('4');
+  expect(window.localStorage.getItem('count')).toBe('4');
+});
